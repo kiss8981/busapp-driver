04ad5dc4b62b2aa096e92438d2e7b364f4fc0952
kiss8981 <kdh89811@gmail.com>
2024-02-21 06:25:47 +0900

    fix: maps google api key

---
+++ app.config.js
@@ -0,0 +1,69 @@
+export default {
+  expo: {
+    name: "스쿨버스 기사님",
+    slug: "busdriver",
+    version: "1.0.1",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: false,
+      bundleIdentifier: "kr.codest.busdriver",
+      buildNumber: "1.0.1",
+      infoPlist: {
+        NSLocationAlwaysUsageDescription:
+          "버스 위치 정보 공유를 위해 사용자의 위치 정보에 접근합니다.",
+        NSLocationWhenInUseUsageDescription:
+          "버스 위치 정보 공유를 위해 사용자의 위치 정보에 접근합니다.",
+        CFBundleDevelopmentRegion: "ko",
+      },
+      config: {
+        usesNonExemptEncryption: false,
+        googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY_IOS,
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      package: "kr.codest.busdriver",
+      versionCode: 2,
+      permissions: [
+        "ACCESS_COARSE_LOCATION",
+        "ACCESS_FINE_LOCATION",
+        "com.google.android.providers.gsf.permission.READ_GSERVICES",
+        "android.permission.INTERNET",
+        "android.permission.ACCESS_COARSE_LOCATION",
+        "android.permission.ACCESS_FINE_LOCATION",
+        "android.permission.FOREGROUND_SERVICE",
+      ],
+      config: {
+        googleMaps: {
+          apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY_ANDROID,
+        },
+      },
+    },
+    plugins: [
+      [
+        "expo-location",
+        {
+          locationAlwaysAndWhenInUsePermission:
+            "위치 정보 공유를 위해 사용자의 위치 정보에 접근합니다.",
+        },
+      ],
+      "expo-secure-store",
+    ],
+    extra: {
+      eas: {
+        projectId: "4340dc9f-81c2-41b0-93d2-e3f98d5b887f",
+      },
+    },
+  },
+};
