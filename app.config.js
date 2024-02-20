9060e9b266911a635ac16d8d9ea2538e68396f0d
kiss8981 <kdh89811@gmail.com>
2024-02-21 07:17:56 +0900

    fix: permission

--- app.config.js
+++ app.config.js
@@ -42,7 +42,6 @@ export default {
         "android.permission.INTERNET",
         "android.permission.ACCESS_COARSE_LOCATION",
         "android.permission.ACCESS_FINE_LOCATION",
-        "android.permission.FOREGROUND_SERVICE",
       ],
       config: {
         googleMaps: {
